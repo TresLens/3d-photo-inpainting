9f00a0a7881d018f5177e33100d57ed263d67da9
cTatu <cristian.tatu@estudiante.uam.es>
2021-07-11 00:26:49 +0200

    modularize

---
+++ image3D.py
@@ -0,0 +1,125 @@
+import numpy as np
+import argparse
+import os
+from functools import partial
+import vispy
+import scipy.misc as misc
+import yaml
+import time
+from mesh import write_ply, read_ply, output_3d_photo
+from utils3D import get_MiDaS_samples, read_MiDaS_depth
+import torch
+import cv2
+from skimage.transform import resize
+import imageio
+import copy
+from networks import Inpaint_Color_Net, Inpaint_Depth_Net, Inpaint_Edge_Net
+from MiDaS.run import run_depth
+from MiDaS.monodepth_net import MonoDepthNet
+import MiDaS.MiDaS_utils as MiDaS_utils
+from bilateral_filtering import sparse_bilateral_filtering
+
+class Image3D:
+    def __init__(self, config) -> None:
+        self.config = yaml.load(open(config, 'r'))
+        vispy.use(app='pyqt5')
+
+        if isinstance(self.config['gpu_ids'], int) and (self.config['gpu_ids'] >= 0):
+            self.device = self.config['gpu_ids']
+        else:
+            self.device = "cpu"
+
+        print(f"Loading edge model at {time.time()}")
+        self.depth_edge_model = Inpaint_Edge_Net(init_weights=True)
+        self.depth_edge_weight = torch.load(self.config['depth_edge_model_ckpt'],
+                                       map_location=torch.device(self.device))
+        self.depth_edge_model.load_state_dict(self.depth_edge_weight)
+        self.depth_edge_model = self.depth_edge_model.to(self.device)
+        self.depth_edge_model.eval()
+
+        print(f"Loading depth model at {time.time()}")
+        self.depth_feat_model = Inpaint_Depth_Net()
+        self.depth_feat_weight = torch.load(self.config['depth_feat_model_ckpt'],
+                                       map_location=torch.device(self.device))
+        self.depth_feat_model.load_state_dict(self.depth_feat_weight, strict=True)
+        self.depth_feat_model = self.depth_feat_model.to(self.device)
+        self.depth_feat_model.eval()
+        self.depth_feat_model = self.depth_feat_model.to(self.device)
+
+        print(f"Loading rgb model at {time.time()}")
+        self.rgb_model = Inpaint_Color_Net()
+        self.rgb_feat_weight = torch.load(self.config['rgb_feat_model_ckpt'],
+                                     map_location=torch.device(self.device))
+        self.rgb_model.load_state_dict(self.rgb_feat_weight)
+        self.rgb_model.eval()
+        self.rgb_model = self.rgb_model.to(self.device)
+        self.graph = None
+
+
+    def run_3dimage(self, src) -> str:
+        src_folder = f"{src}/image"
+        self.config['mesh_folder'] = src
+        self.config['video_folder'] = src
+        self.config['depth_folder'] = src
+
+        sample_list = get_MiDaS_samples(src_folder, self.config['depth_folder'], self.config, self.config['specific'])
+        normal_canvas, all_canvas = None, None
+
+        for idx in range(len(sample_list)):
+            depth = None
+            sample = sample_list[idx]
+            print("Current Source ==> ", sample['src_pair_name'])
+            mesh_fi = os.path.join(self.config['mesh_folder'], sample['src_pair_name'] +'.ply')
+            image = imageio.imread(sample['ref_img_fi'])
+            
+            self.config['output_h'], self.config['output_w'] = imageio.imread(sample['depth_fi']).shape[:2]
+            
+            frac = self.config['longer_side_len'] / max(self.config['output_h'], self.config['output_w'])
+            self.config['output_h'], self.config['output_w'] = int(self.config['output_h'] * frac), int(self.config['output_w'] * frac)
+            self.config['original_h'], self.config['original_w'] = self.config['output_h'], self.config['output_w']
+            if image.ndim == 2:
+                image = image[..., None].repeat(3, -1)
+            if np.sum(np.abs(image[..., 0] - image[..., 1])) == 0 and np.sum(np.abs(image[..., 1] - image[..., 2])) == 0:
+                self.config['gray_image'] = True
+            else:
+                self.config['gray_image'] = False
+            image = cv2.resize(image, (self.config['output_w'], self.config['output_h']), interpolation=cv2.INTER_AREA)
+            depth = read_MiDaS_depth(sample['depth_fi'], 3.0, self.config['output_h'], self.config['output_w'])
+            mean_loc_depth = depth[depth.shape[0]//2, depth.shape[1]//2]
+            if not(self.config['load_ply'] is True and os.path.exists(mesh_fi)):
+                _, vis_depths = sparse_bilateral_filtering(depth.copy(), image.copy(), self.config, num_iter=self.config['sparse_iter'], spdb=False)
+                depth = vis_depths[-1]
+                torch.cuda.empty_cache()
+                print("Start Running 3D_Photo ...")
+        
+                print(f"Writing depth ply (and basically doing everything) at {time.time()}")
+                rt_info = write_ply(image,
+                                    depth,
+                                    sample['int_mtx'],
+                                    mesh_fi,
+                                    self.config,
+                                    self.rgb_model,
+                                    self.depth_edge_model,
+                                    self.depth_edge_model,
+                                    self.depth_feat_model)
+
+                if rt_info is False:
+                    continue
+                
+                torch.cuda.empty_cache()
+            if self.config['save_ply'] is True or self.config['load_ply'] is True:
+                verts, colors, faces, Height, Width, hFov, vFov = read_ply(mesh_fi)
+            else:
+                verts, colors, faces, Height, Width, hFov, vFov = rt_info
+
+            print(f"Making video at {time.time()}")
+            videos_poses, video_basename = copy.deepcopy(sample['tgts_poses']), sample['tgt_name']
+            top = (self.config.get('original_h') // 2 - sample['int_mtx'][1, 2] * self.config['output_h'])
+            left = (self.config.get('original_w') // 2 - sample['int_mtx'][0, 2] * self.config['output_w'])
+            down, right = top + self.config['output_h'], left + self.config['output_w']
+            border = [int(xx) for xx in [top, down, left, right]]
+            normal_canvas, all_canvas = output_3d_photo(verts.copy(), colors.copy(), faces.copy(), copy.deepcopy(Height), copy.deepcopy(Width), copy.deepcopy(hFov), copy.deepcopy(vFov),
+                                copy.deepcopy(sample['tgt_pose']), sample['video_postfix'], copy.deepcopy(sample['ref_pose']), copy.deepcopy(self.config['video_folder']),
+                                image.copy(), copy.deepcopy(sample['int_mtx']), self.config, image,
+                                videos_poses, video_basename, self.config.get('original_h'), self.config.get('original_w'), border=border, depth=depth, normal_canvas=normal_canvas, all_canvas=all_canvas,
+                                mean_loc_depth=mean_loc_depth)
---
+++ monocularDepth.py
@@ -0,0 +1,183 @@
+import sys
+import os
+sys.path.append(os.getcwd() + '/BoostingMonocularDepth')
+
+from operator import getitem
+from torchvision.transforms import Compose
+from torchvision.transforms import transforms
+
+# OUR
+from BoostingMonocularDepth.utils import ImageandPatchs, ImageDataset, generatemask, getGF_fromintegral, calculateprocessingres, rgb2gray,\
+    applyGridpatch
+
+# MIDAS
+import BoostingMonocularDepth.midas.utils
+from BoostingMonocularDepth.midas.models.midas_net import MidasNet
+from BoostingMonocularDepth.midas.models.transforms import Resize, NormalizeImage, PrepareForNet
+
+# PIX2PIX : MERGE NET
+from BoostingMonocularDepth.pix2pix.options.test_options import TestOptions
+from BoostingMonocularDepth.pix2pix.models.pix2pix4depth_model import Pix2Pix4DepthModel
+
+from BoostingMonocularDepth.run import doubleestimate, generatepatchs
+
+import time
+import torch
+import cv2
+import numpy as np
+import argparse
+import warnings
+warnings.simplefilter('ignore', np.RankWarning)
+
+class MonocularDepth:
+    def __init__(self, depthNet = 0) -> None:
+        self.device = torch.device("cuda")
+        self.whole_size_threshold = 3000  # R_max from the paper
+        self.GPU_threshold = 500 - 32 # Limit for the GPU (NVIDIA RTX 2080), can be adjusted
+        self.pix2pixsize = 1024
+        self.depthNet = depthNet
+
+        # Load merge network
+        opt = TestOptions().parse()
+        self.pix2pixmodel = Pix2Pix4DepthModel(opt)
+        self.pix2pixmodel.save_dir = 'BoostingMonocularDepth/pix2pix/checkpoints/mergemodel'
+        self.pix2pixmodel.load_networks('latest')
+        self.pix2pixmodel.eval()
+
+        if depthNet == 0:
+            self.net_receptive_field_size = 384
+            self.patch_netsize = 2*self.net_receptive_field_size
+        elif depthNet == 1:
+            self.net_receptive_field_size = 448
+            self.patch_netsize = 2*self.net_receptive_field_size
+
+        # Decide which depth estimation network to load
+        if depthNet == 0:
+            midas_model_path = "BoostingMonocularDepth/midas/model.pt"
+            self.midasmodel = MidasNet(midas_model_path, non_negative=True)
+            self.midasmodel.to(self.device)
+            self.midasmodel.eval()
+        elif depthNet == 1:
+            from BoostingMonocularDepth.structuredrl.models import DepthNet
+            self.srlnet = DepthNet.DepthNet()
+            self.srlnet = torch.nn.DataParallel(self.srlnet, device_ids=[0]).cuda()
+            checkpoint = torch.load('BoostingMonocularDepth/structuredrl/model.pth.tar')
+            self.srlnet.load_state_dict(checkpoint['state_dict'])
+            self.srlnet.eval()
+
+        self.mask_org = generatemask((3000, 3000))
+        self.mask = self.mask_org.copy()
+
+        self.r_threshold_value = 0.2
+
+    def run_monocularDepth(self, src_dir, result_dir) -> None:
+        dataset = ImageDataset(src_dir, 'test')
+        for image_ind, images in enumerate(dataset):
+            img = images.rgb_image
+            input_resolution = img.shape
+
+            scale_threshold = 3  # Allows up-scaling with a scale up to 3
+
+            whole_image_optimal_size, patch_scale = calculateprocessingres(img, self.net_receptive_field_size,
+                                                                       self.r_threshold_value, scale_threshold,
+                                                                       self.whole_size_threshold)
+
+            whole_estimate = doubleestimate(img, self.net_receptive_field_size, whole_image_optimal_size,
+                                        self.pix2pixsize, self.depthNet, self.pix2pixmodel, self.GPU_threshold, midasmodel=self.midasmodel)
+
+            self.factor = max(min(1, 4 * patch_scale * whole_image_optimal_size / self.whole_size_threshold), 0.2)
+
+            if img.shape[0] > img.shape[1]:
+                a = 2*whole_image_optimal_size
+                b = round(2*whole_image_optimal_size*img.shape[1]/img.shape[0])
+            else:
+                a = round(2*whole_image_optimal_size*img.shape[0]/img.shape[1])
+                b = 2*whole_image_optimal_size
+
+            img = cv2.resize(img, (round(b/self.factor), round(a/self.factor)), interpolation=cv2.INTER_CUBIC)
+
+            base_size = self.net_receptive_field_size*2
+
+            patchset = generatepatchs(img, base_size, self.factor)
+
+            mergin_scale = input_resolution[0] / img.shape[0]
+
+            imageandpatchs = ImageandPatchs(src_dir, images.name, patchset, img, mergin_scale)
+            whole_estimate_resized = cv2.resize(whole_estimate, (round(img.shape[1]*mergin_scale),
+                                                round(img.shape[0]*mergin_scale)), interpolation=cv2.INTER_CUBIC)
+            imageandpatchs.set_base_estimate(whole_estimate_resized.copy())
+            imageandpatchs.set_updated_estimate(whole_estimate_resized.copy())
+
+            for patch_ind in range(len(imageandpatchs)):
+                self.__process_patch(patch_ind, imageandpatchs)
+            
+            path = os.path.join(result_dir, imageandpatchs.name)
+
+            BoostingMonocularDepth.midas.utils.write_depth(path,
+                                    cv2.resize(imageandpatchs.estimation_updated_image,
+                                               (input_resolution[1], input_resolution[0]),
+                                               interpolation=cv2.INTER_CUBIC), bits=2, colored=False)
+
+    def __process_patch(self, patch_ind, imageandpatchs):
+        patch = imageandpatchs[patch_ind] # patch object
+        patch_rgb = patch['patch_rgb'] # rgb patch
+        patch_whole_estimate_base = patch['patch_whole_estimate_base'] # corresponding patch from base
+        rect = patch['rect'] # patch size and location
+        patch_id = patch['id'] # patch ID
+        org_size = patch_whole_estimate_base.shape # the original size from the unscaled input
+
+        # We apply double estimation for patches. The high resolution value is fixed to twice the receptive
+        # field size of the network for patches to accelerate the process.
+        patch_estimation = doubleestimate(patch_rgb, self.net_receptive_field_size, self.patch_netsize,
+                                        self.pix2pixsize, self.depthNet, self.pix2pixmodel, self.GPU_threshold, midasmodel=self.midasmodel)
+
+        patch_estimation = cv2.resize(patch_estimation, (self.pix2pixsize, self.pix2pixsize),
+                                    interpolation=cv2.INTER_CUBIC)
+
+        patch_whole_estimate_base = cv2.resize(patch_whole_estimate_base, (self.pix2pixsize, self.pix2pixsize),
+                                            interpolation=cv2.INTER_CUBIC)
+
+        # Merging the patch estimation into the base estimate using our merge network:
+        # We feed the patch estimation and the same region from the updated base estimate to the merge network
+        # to generate the target estimate for the corresponding region.
+        self.pix2pixmodel.set_input(patch_whole_estimate_base, patch_estimation)
+
+        # Run merging network
+        self.pix2pixmodel.test()
+        visuals = self.pix2pixmodel.get_current_visuals()
+
+        prediction_mapped = visuals['fake_B']
+        prediction_mapped = (prediction_mapped+1)/2
+        prediction_mapped = prediction_mapped.squeeze().cpu().numpy()
+
+        mapped = prediction_mapped
+
+        # We use a simple linear polynomial to make sure the result of the merge network would match the values of
+        # base estimate
+        p_coef = np.polyfit(mapped.reshape(-1), patch_whole_estimate_base.reshape(-1), deg=1)
+        merged = np.polyval(p_coef, mapped.reshape(-1)).reshape(mapped.shape)
+
+        merged = cv2.resize(merged, (org_size[1],org_size[0]), interpolation=cv2.INTER_CUBIC)
+
+        # Get patch size and location
+        w1 = rect[0]
+        h1 = rect[1]
+        w2 = w1 + rect[2]
+        h2 = h1 + rect[3]
+
+        # To speed up the implementation, we only generate the Gaussian mask once with a sufficiently large size
+        # and resize it to our needed size while merging the patches.
+        if self.mask.shape != org_size:
+            mask = cv2.resize(self.mask_org, (org_size[1],org_size[0]), interpolation=cv2.INTER_LINEAR)
+
+        tobemergedto = imageandpatchs.estimation_updated_image
+
+        # Update the whole estimation:
+        # We use a simple Gaussian mask to blend the merged patch region with the base estimate to ensure seamless
+        # blending at the boundaries of the patch region.
+        tobemergedto[h1:h2, w1:w2] = np.multiply(tobemergedto[h1:h2, w1:w2], 1 - mask) + np.multiply(merged, mask)
+        imageandpatchs.set_updated_estimate(tobemergedto)
+
+if __name__ == '__main__':
+    md = MonocularDepth(0)
+    md.run_monocularDepth('image/image', 'image/frames')
